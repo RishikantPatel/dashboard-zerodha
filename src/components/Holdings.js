03f8e054653478d16d64f2e3b994b97ee5149a97
Rishikant Patel <patelrishikant88@gamil.com>
2026-02-07 18:29:58 +0530

    fix eslint accessibility errors

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://backend-zerodha-gamma.vercel.app/").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
